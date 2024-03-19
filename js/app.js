95bcfa66c400afd72cc9337e24be4774cbfcbd85
Jake Spencer <various@jakespencer.net>
2024-03-19 14:45:05 -0500

    debug

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ console.log('I am running!');
 
 const myh = new Headers();
 myh.append('Authorization', 'Token 630191c8aece95e275320509641cbc887a117019')
-const myInit = {method: "GET", headers, myh}
+const myInit = {method: "GET", headers: myh}
 const request = new Request('https://api.esv.org/v3/passage/audio/?q=Psalm+146', myInit)
 
 fetch(request)
